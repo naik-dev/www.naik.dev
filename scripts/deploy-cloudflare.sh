58d165339464f11aafdeacb0ca4472bf7ff4c162
Shreyas Naik <shrnaik@amazon.com>
2026-09-18 10:14:45 +0100

    Add search and social metadata

--- scripts/deploy-cloudflare.sh
+++ scripts/deploy-cloudflare.sh
@@ -13,7 +13,7 @@ trap cleanup EXIT
 cd "$project_root"
 ./scripts/check.sh
 
-cp index.html styles.css app.js favicon.svg "$staging_dir/"
+cp index.html styles.css app.js favicon.svg robots.txt sitemap.xml social-card.svg "$staging_dir/"
 
 commit_hash="$(git rev-parse --short HEAD)"
 commit_message="$(git log -1 --pretty=%s)"
@@ -25,4 +25,3 @@ CLOUDFLARE_ACCOUNT_ID="206af19c8ad658c40528f31e39cf6ca6" \
   --commit-hash "$commit_hash" \
   --commit-message "$commit_message" \
   --commit-dirty=true
-
